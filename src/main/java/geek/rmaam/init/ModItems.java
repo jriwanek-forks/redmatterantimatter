fc2c37512e029817257374fc3c83e403e6b82c90
LegendaryGeek <timothydaley4@gmail.com>
2019-10-16 16:09:49 -0400

    temp items for the mod

--- src/main/java/geek/rmaam/init/ModItems.java
+++ src/main/java/geek/rmaam/init/ModItems.java
@@ -1,9 +1,11 @@
 package geek.rmaam.init;
 
+import net.minecraft.item.Item;
+
 public class ModItems {
 
-	public ModItems() {
-		// TODO Auto-generated constructor stub
-	}
+	public static final Item ThePredicament = new Item();
+	public static final Item RedMatter = new Item();
+	public static final Item AntiMatter = new Item();
 
 }
