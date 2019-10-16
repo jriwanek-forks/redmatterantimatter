84c47f357f16aac3b496776d6b9ceb0413464358
LegendaryGeek <timothydaley4@gmail.com>
2019-10-16 16:15:01 -0400

    Item Classes for the items, still no functionality yet

--- src/main/java/geek/rmaam/items/AntiMatter.java
+++ src/main/java/geek/rmaam/items/AntiMatter.java
@@ -3,19 +3,19 @@ package geek.rmaam.items;
 import net.minecraft.util.text.TextFormatting;
 import slimeknights.tconstruct.library.materials.Material;
 
-public class RedMatter extends Material {
+public class AntiMatter extends Material {
 
-	public RedMatter(String identifier, TextFormatting textColor) {
+	public AntiMatter(String identifier, TextFormatting textColor) {
 		super(identifier, textColor);
 		// TODO Auto-generated constructor stub
 	}
 
-	public RedMatter(String identifier, int color) {
+	public AntiMatter(String identifier, int color) {
 		super(identifier, color);
 		// TODO Auto-generated constructor stub
 	}
 
-	public RedMatter(String identifier, int color, boolean hidden) {
+	public AntiMatter(String identifier, int color, boolean hidden) {
 		super(identifier, color, hidden);
 		// TODO Auto-generated constructor stub
 	}
---
+++ src/main/java/geek/rmaam/items/ThePredicament.java
@@ -0,0 +1,9 @@
+package geek.rmaam.items;
+
+public class ThePredicament {
+
+	public ThePredicament() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
