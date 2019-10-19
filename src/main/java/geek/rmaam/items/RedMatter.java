1b78e4a18f965b5244bd207b7ea47e35934ee334
LegendaryGeek <timothydaley4@gmail.com>
2019-10-19 12:24:35 -0400

    Red Matter TiCon Material

--- src/main/java/geek/rmaam/items/RedMatter.java
+++ src/main/java/geek/rmaam/items/RedMatter.java
@@ -4,20 +4,14 @@ import net.minecraft.util.text.TextFormatting;
 import slimeknights.tconstruct.library.materials.Material;
 
 public class RedMatter extends Material {
+	
+	private static final String rmidentifier = "redmatter";
 
-	public RedMatter(String identifier, TextFormatting textColor) {
-		super(identifier, textColor);
-		// TODO Auto-generated constructor stub
+	public RedMatter() {
+		super(rmidentifier, TextFormatting.RED);
+		this.setCastable(true);
+		this.setCraftable(true);
 	}
 
-	public RedMatter(String identifier, int color) {
-		super(identifier, color);
-		// TODO Auto-generated constructor stub
-	}
-
-	public RedMatter(String identifier, int color, boolean hidden) {
-		super(identifier, color, hidden);
-		// TODO Auto-generated constructor stub
-	}
 
 }
