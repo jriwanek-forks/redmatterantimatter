fef4d6bf3c54cf68c8cdc798a311565f9f553cd6
LegendaryGeek <timothydaley4@gmail.com>
2019-10-19 12:24:07 -0400

    Antimatter TiCon material

--- src/main/java/geek/rmaam/items/AntiMatter.java
+++ src/main/java/geek/rmaam/items/AntiMatter.java
@@ -5,19 +5,13 @@ import slimeknights.tconstruct.library.materials.Material;
 
 public class AntiMatter extends Material {
 
-	public AntiMatter(String identifier, TextFormatting textColor) {
-		super(identifier, textColor);
-		// TODO Auto-generated constructor stub
+	private static final String amidentifier = "antimatter";
+
+	public AntiMatter() {
+		super(amidentifier, TextFormatting.BLACK);
+		this.setCastable(true);
+		this.setCraftable(true);
 	}
 
-	public AntiMatter(String identifier, int color) {
-		super(identifier, color);
-		// TODO Auto-generated constructor stub
-	}
-
-	public AntiMatter(String identifier, int color, boolean hidden) {
-		super(identifier, color, hidden);
-		// TODO Auto-generated constructor stub
-	}
 
 }
